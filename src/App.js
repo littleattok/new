2005b658e950a20ef7169a9c5ca9149c17009068
littleattok <difj1324@gmail.com>
2025-02-02 00:53:46 +0900

    state 사용용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+
 
 function Header(props) {          //대문자로 정의해야함
   console.log(props);
@@ -22,7 +25,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
       <a id={t.id} href={"/read/"+t.id} onClick={event=>{
       event.preventDefault();
-      props.onChangeMode(event.target.id);
+      props.onChangeMode(Number(event.target.id));
     }}>{t.title}</a></li>)
   }
   
@@ -39,21 +42,45 @@ function Article(props) {
   return (
     <article>
       <h2>{props.title}</h2>
-      {props.body}, WEB!
+      {props.body}
     </article>
   )
 }
+
+
+
 function App() {
   const topics =[
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'js', body:'js is ...'}
-  ];
+    {id:3, title:'js', body:'js is ...'}];
+
+  // const _mode= useState('WELCOME');
+  // const mode= _mode[0];
+  // const setMode=_mode[1];
+    const [mode, setMode]= useState('WELCOME');
+    const [id, setId]= useState(null);
+
+  let content =null;
+  if(mode==='WELCOME'){
+    content= <Article title="Welcome" body="hello, web"></Article>
+  }
+  else if(mode=== 'READ'){
+    let title , body= null;
+    for(let i = 0; i< topics.length; i++){
+      if (topics[i].id===id){
+        title =topics[i].title;
+        body= topics[i].body;
+      }
+    }
+    content= <Article title={title} body={body}></Article>
+  }
+
   return (
     <div>
-      <Header title="My React App" onChangeMode={function(){alert('hi')}}></Header>
-      <Nav topics ={topics} onChangeMode={(id)=>{alert(id);}}></Nav>
-      <Article title="WWWelcome" body="hello"></Article>
+      <Header title="My React App" onChangeMode={()=>{setMode('WELCOME');}}></Header>
+      <Nav topics ={topics} onChangeMode={(id)=>{setMode('READ'); setId(id);}}></Nav>
+      {content}
     </div>
   );
 }
